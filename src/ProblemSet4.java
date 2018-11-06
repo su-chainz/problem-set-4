fb32808a56f11b36922df2fb29d4aaf31022954c
Suchetan Dontha <suchdon777@gmail.com>
2018-11-06 07:56:31 -0500

    update

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -148,4 +148,97 @@ public class ProblemSet4 {
 		}
 		return false;
 	}
+	
+	public int triplets(String input) {
+		int inputLength = input.length();
+		boolean whitespaceCheck = false;
+		boolean letterCheck = false;
+		char first = 'a';
+		char second = 'b';
+		char third = 'c';
+		int tripletCount = 0;
+		for (int i = 0; i < inputLength; i++) {
+			if (input.charAt(i) == ' ') {
+				whitespaceCheck = true;
+			}
+		}
+		for (int g = 0; g < inputLength; g++) {
+			if (Character.isDigit(input.charAt(g)) == true) {
+				letterCheck = true;
+			}
+		}
+		if (input == null || whitespaceCheck == true || letterCheck == true) {
+			System.out.println("failed");
+			return -1;
+		}
+		for (int h = 0; h < inputLength; h++) {
+			if (h+2 < inputLength) {
+				first = input.charAt(h);
+				second = input.charAt(h+1);
+				third = input.charAt(h+2);
+				if (first == second && second == third ) {
+					tripletCount += 1;
+				}
+			}
+		}
+		System.out.println(tripletCount);
+		return tripletCount;
+	}
+	
+	
+	public int addMe(String input, boolean add) {
+		char jeff = ' ';
+		int numberStart = 0;
+		int numberEnd = 1;
+		String number = null;
+		int sum = 0;
+		int numberReal = 0;
+		boolean whitespaceCheck = false;
+		int digit = 0;
+		int inputLength = input.length();
+		for (int i = 0; i < inputLength; i++) {
+			if (input.charAt(i) == ' ') {
+				whitespaceCheck = true;
+			}
+		}
+		if (input == null || whitespaceCheck == true) {
+			return -1;
+		}
+		if (add == true) {
+			for (int g = 0; g < inputLength; g++) {
+				if (Character.isDigit(input.charAt(g)) == true) {
+					jeff = input.charAt(g);
+					digit = jeff - '0';
+					sum += digit;
+				}
+			}
+		}
+		else {
+			for (int h = 0; h <inputLength; h++) {
+				if (Character.isDigit(input.charAt(h)) == true) {
+					numberStart = h;
+					for (int j = h; j<inputLength; j++) {
+						if(Character.isLetter(input.charAt(j)) == true && j < inputLength-1) {
+							numberEnd = j;
+							break;
+						}
+						else if (j == inputLength-1 && Character.isDigit(input.charAt(inputLength-1)) == true) {
+							numberEnd = inputLength;
+							break;
+						}
+					}
+					number = input.substring(numberStart, numberEnd);
+					numberReal = Integer.parseInt(number);
+					System.out.println(numberReal);
+					sum += numberReal;
+					h = numberEnd;
+				}
+			}
+			
+		}
+		System.out.println(sum);
+		return sum;
+		
+}
+
 }
